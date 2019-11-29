4662a67562104fa301669dff2ca8d4c3541262b8
mortalliao <mortalliaoyujian@163.com>
2019-11-30 05:56:25 +0800

    add demo

--- src/main/java/com/sourcecode/learning/mybatis/entity/User.java
+++ src/main/java/com/sourcecode/learning/mybatis/entity/User.java
@@ -8,10 +8,8 @@ import lombok.Data;
 @Data
 public class User implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -7591167926253352944L;
+	private static final long serialVersionUID = -7644967201570973657L;
+	
 	private Long id;
 	private String account;
 	private String password;
@@ -20,5 +18,5 @@ public class User implements Serializable {
 	private Integer age;
 	private Date birthday;
 	private Date createTime;
-
+	
 }
--- src/test/java/com/sourcecode/learning/mybatis/MybatisTest.java
+++ src/test/java/com/sourcecode/learning/mybatis/MybatisTest.java
@@ -1,19 +1,55 @@
 package com.sourcecode.learning.mybatis;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.io.VFS;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import lombok.extern.slf4j.Slf4j;
+import com.sourcecode.learning.mybatis.entity.User;
 
-@Slf4j
 public class MybatisTest {
 
-//	D:\develop\IDE\Work_Space\eclipse_jee_neon\mybatis\src\main\java\cn\itcast\mybatis\pojo
 	@Test
-	public void test() {
-		log.debug("debug");
-		log.info("info");
-		log.error("error");
-		log.trace("trace");
-		log.warn("warn");
+	public void getSqlSessionFactoryWithXML() throws IOException {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		System.out.println(sqlSessionFactory);
+
+		// WithoutXML
+//		DataSource dataSource = DataSourceFactory.getDataSource();
+//		TransactionFactory transactionFactory = new JdbcTransactionFactory();
+//		Environment environment = new Environment("development", transactionFactory, dataSource);
+//		Configuration configuration = new Configuration(environment);
+//		configuration.addMapper(BlogMapper.class);
+//		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
 	}
+
+	@Test
+	public void testMybatis() throws IOException {
+		// mybatis的总配置文件路径
+		String resource = "mybatis-config.xml";
+		// 利用mybatis的Resources工具类获取配置文件输入流
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		// 利用SqlSessionFactoryBuilder根据mybatis配置文件输入流创建SqlSessionFactory
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+		// 根据sqlSessionFactory获取SqlSession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+
+		// 持久化操作
+		// selectOne 参数1：在映射文件中对应的statement语句（包括文件的空间和语句id）
+		// selectOne 参数2：在映射文件中对应的statement语句中对应的输入参数
+		User user = sqlSession.selectOne("xxx.queryUserById", 1L);
+
+		System.out.println(user);
+
+		sqlSession.close();
+	}
+
 }
